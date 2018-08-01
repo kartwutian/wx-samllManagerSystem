dc03a06754776446184608c20d684677cb15f3c9
bhz <baohz@zjbdos.com>
2018-08-01 17:44:52 +0800

    third day

--- pages/projectProgressOperate/index.js
+++ pages/projectProgressOperate/index.js
@@ -31,7 +31,6 @@ Page({
       }
     });
   },
-
   toggleShowMore() {
     this.setData({
       showMore: !this.data.showMore,
@@ -57,13 +56,17 @@ Page({
   handleSelect() {
     console.log(111111)
   },
-
-  handleNavigator() {
-    wx.navigateTo({
-      url: '/pages/projectProgressOperate/index',
+  chooseImage: function (e) {
+    wx.chooseImage({
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success: (res) => {
+        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+        this.setData({
+          files: this.data.files.concat(res.tempFilePaths)
+        });
+      }
     })
   },
-  test() {
-    console.log(222222)
-  }
+
 });
